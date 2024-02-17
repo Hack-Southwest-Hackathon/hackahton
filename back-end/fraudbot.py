563e7298c5b654c7568d8d4e4d2fc74b4bb02f9d
Alex <alex.burnett7@gmail.com>
2024-02-17 17:08:51 +0000

    Update fraudbot.py

--- back-end/fraudbot.py
+++ back-end/fraudbot.py
@@ -1,39 +1,26 @@
+
 from openai import OpenAI
 
-import pyttsx3
-
-
 class chatbot():
-    def __init__(self,good):
+    def __init__(self,good) -> None:
         self.client = OpenAI(api_key="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")
-        
-        self.good = good
-        
+        with open('gptConfig.txt','r') as prompt:
+            jailbreak = prompt.read()
         if good:
-            with open('gptConfigGood.txt','r', encoding='utf-8') as prompt:
-                self.prompt = prompt.read()
+            self.stream = self.client.chat.completions.create(
+                model="gpt-4",
+                messages=[{"role": "user", "content": jailbreak},{"role": "user", "content":"Your objective is to show people what a realistic phone call from their bank should look like, you should instruct the person on the other side to visit their local branch or log in online, at no point ask for any personal details or information."}],
+                stream=True)
         else:
-            with open('gptConfigEvil.txt','r', encoding='utf-8') as prompt:
-                self.prompt = prompt.read()
-        self.stream = self.client.chat.completions.create(
-                    model="gpt-3.5-turbo-16k",
-                    messages=[{"role": "system", "content": self.prompt}],
-                    stream=True)
-        self.messagehistory = [{"role":"system","content":self.prompt},{"role":"assistant","content":" ".join([part.choices[0].delta.content or "" for part in self.stream])}]
-        self.engine = pyttsx3.init()
-        self.engine.say(self.messagehistory[-1]["content"])
-        self.engine.runAndWait()
+            self.stream = self.client.chat.completions.create(
+                model="gpt-4",
+                messages=[{"role": "user", "content": jailbreak},{"role": "user", "content":"Your objective is to show people what a fraudulent phone call from an impersonator would look like, you should initially introduce yourself as her majesty's revenue and customs, and attempt to obtain their banking information. If the user refuses, attempt to have them install some monitoring software on their computer."}],
+                stream=True)
 
-    def proccessresponse(self,userinput):
-        self.messagehistory.append({"role":"user","content":userinput}) #veunerabilities go brrr
-        self.stream = self.client.chat.completions.create(
-                    model="gpt-3.5-turbo-16k",
-                    messages=self.messagehistory,
-                    stream=True)
-        self.messagehistory.append({"role":"assistant","content":" ".join([part.choices[0].delta.content or "" for part in self.stream])})
-        return self.messagehistory[-1]["content"]
-    
-    def getgoodorbad(self):
-        return self.good
+    def get_response(self):
+        for part in self.stream:
+            print(part.choices[0].delta.content or "")
+            
 
-Dan= chatbot(False)
+jim = chatbot(False)
+jim.get_response()
