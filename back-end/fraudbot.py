2efe96d26de36317edbad1123acda5532593f89a
Edward Pratt <egwpmpp9@gmail.com>
2024-02-17 22:20:37 +0000

    Max Tokens = 100

--- back-end/fraudbot.py
+++ back-end/fraudbot.py
@@ -1,12 +1,13 @@
 from openai import OpenAI
 import os
 
+
 class chatbot():
-    def __init__(self, mode:str) -> None:
+    def __init__(self, mode: str) -> None:
         '''
         Initialise an instance of gpt 3.5 and
         set it to a certain prompted build.
-        
+
         Options atm are:
         Housing
         Visa
@@ -14,46 +15,46 @@ class chatbot():
         '''
         self.client = OpenAI(
             api_key="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"
-            )
+        )
 
         self.mode = mode
         with open(
             os.path.dirname(
                 os.path.abspath(__file__)
-                ) + '/AI Prompts/' + self.mode + '.txt', 'r', encoding='utf-8'
-            ) as prompt:
+            ) + '/AI Prompts/' + self.mode + '.txt', 'r', encoding='utf-8'
+        ) as prompt:
             self.prompt = prompt.read()
-        
+
         self.stream = self.client.chat.completions.create(
             model="gpt-3.5-turbo-16k",
             messages=[{
                 "role": "system",
                 "content": self.prompt
-                }],
-            max_tokens=200,
+            }],
+            max_tokens=100,
             stream=True)
-        
+
         self.messagehistory = [{
             "role": "system", "content": self.prompt
-            },{
-                "role": "assistant",
-                "content": "".join([
+        }, {
+            "role": "assistant",
+            "content": "".join([
                     part.choices[0].delta.content or "" for part in self.stream
-                    ])}]
+            ])}]
 
     def getfirstmessage(self) -> str:
         '''
         Returns the initial message by the 
         chatbot when it is first opened
         '''
-        
+
         return self.messagehistory[
             -1
-            ][
-                "content"
-                ]
+        ][
+            "content"
+        ]
 
-    def proccessresponse(self, userinput:str) -> str:
+    def proccessresponse(self, userinput: str) -> str:
         '''
         Process and return user
         response
@@ -62,19 +63,19 @@ class chatbot():
         self.messagehistory.append({
             "role": "user",
             "content": userinput
-            })
-        
+        })
+
         self.stream = self.client.chat.completions.create(
             model="gpt-3.5-turbo-16k",
             messages=self.messagehistory,
-            max_tokens=200,
+            max_tokens=100,
             stream=True)
-        
+
         self.messagehistory.append({
             "role": "assistant",
             "content": " ".join(
-            [part.choices[0].delta.content or "" for part in self.stream])})
-        
+                [part.choices[0].delta.content or "" for part in self.stream])})
+
         return self.messagehistory[-1]["content"]
 
     def getgoodorbad(self) -> bool:
@@ -82,7 +83,6 @@ class chatbot():
         check wether or not the
         mode had malicious intent
         '''
-        if self.mode in ["Bank","visa"]:
+        if self.mode in ["Bank", "visa"]:
             return False
         return True
-    
