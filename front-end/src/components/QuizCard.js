6a4264ba027f324ac01addd2a554f488905e7cf2
duc-minh-droid <ducminhcsp@gmail.com>
2024-02-18 07:05:42 +0000

    bless me

--- front-end/src/components/QuizCard.js
+++ front-end/src/components/QuizCard.js
@@ -1,26 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-function QuizCard({ quiz, val, setVal }) {
+function QuizCard({ quiz, val, setVal, setIsClicked, isClicked }) {
   const handleInputChange = (i) => {
     setVal(i);
+    setIsClicked(true)
   };
 
   return (
-    <div>
-      <h1>{quiz.question}</h1>
+    <div className='px-3'>
+      <h1 className='mb-6 px-2'>{quiz.question}</h1>
       <div>
         {quiz.options.map((option, i) => (
           <div key={i}>
-            <input
-              type="radio"
-              id={option}
-              name="quiz"
-              value={i}
-              checked={val === i}
-              onChange={()=>handleInputChange(i)}
-              required={true}
-            />
-            <label htmlFor={option}>{option}</label>
+            <div onClick={()=>handleInputChange(i)}
+            className={`cursor-pointer ${!isClicked?"quiz-option": val==i?'option-clicked':"quiz-option"}`}>{option}</div>
           </div>
         ))}
       </div>
--- front-end/src/pages/QuizPage.js
+++ front-end/src/pages/QuizPage.js
@@ -1,18 +1,24 @@
 import React, { useEffect, useState } from 'react'
 import QuizCard from '../components/QuizCard'
 import { ThreeDots } from 'react-loading-icons'
+import {useNavigate} from 'react-router-dom'
+import { IoHome } from "react-icons/io5";
+import { FaArrowLeft } from 'react-icons/fa';
 
 // const quizzes = [{question: "Are you gay?", options: ["Yes", "No"], answer: "No"},{question: "Are you homophobic?", options: ["Yes", "No"], answer: "No"},{question: "Is your bitch gay?", options: ["Yes", "No", "She is a femboy"], answer: "No"}]
 
 function QuizPage() {
     const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0)
-    const [currentQuestionAnswer, setCurrentQuestionAnswer] = useState(0)
+    const [currentQuestionAnswer, setCurrentQuestionAnswer] = useState("")
     const [userAnswers, setUserAnswers] = useState([])
     const [quizzes, setQuizzes] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
+    const nav = useNavigate()
+    const [isClicked, setIsClicked] = useState(false)
 
     const nextQuestion = () => {
         setCurrentQuestionAnswer("")
+        setIsClicked(false)
         setUserAnswers(prevAnswers=>[...prevAnswers, currentQuestionAnswer]);
       if (currentQuestionIndex + 1 < quizzes.length) {
         setCurrentQuestionIndex(currentQuestionIndex + 1);
@@ -20,6 +26,7 @@ function QuizPage() {
     };
 
     const sendUserAnswers = async (ans) => {
+        console.log(ans)
         await fetch('http://127.0.0.1:5100/quiz', {
             method: 'POST',
             headers: {
@@ -27,6 +34,7 @@ function QuizPage() {
             },
             body: JSON.stringify(ans),
         })
+        .then(e=>nav('/reflection'))
     }
 
     const handleFinishQuiz = () => {
@@ -48,23 +56,34 @@ function QuizPage() {
         fetchQuizzes();
       }, []);
 
+
     return (
       <div>
+        <div className='flex flex-row justify-around items-center py-5 px-3 w-full'>
+            <FaArrowLeft size={25} onClick={()=>nav(-1)}/>
+            <div className='quizTab'>Preventing Fraudulent Calls</div>
+            <IoHome size={25}/>
+        </div>
         {isLoading || !quizzes.length ? <ThreeDots fill='gray' /> : <>
         <QuizCard
         quiz={quizzes[currentQuestionIndex]}
         val={currentQuestionAnswer}
         setVal={setCurrentQuestionAnswer}
+        setIsClicked={setIsClicked}
+        isClicked={isClicked}
         />
+        <div className='flex w-full justify-end px-5'>
         {currentQuestionIndex < quizzes.length - 1 ? (
-          <button className="mt-4" onClick={nextQuestion}>
+          <button className="mt-4 bg-green-300 border-2 border-green-400 p-1 rounded-full px-3" onClick={nextQuestion}>
             Next Question
           </button>
         ) : (
-            <button className="mt-4" onClick={handleFinishQuiz}>
+            <button className="mt-4 bg-green-300 border-2 border-green-400 p-1 rounded-full px-3" onClick={handleFinishQuiz}>
                 Finish Quiz
             </button>
-        )}</>}
+        )}  
+        </div>
+        </>}
         
       </div>
     );
